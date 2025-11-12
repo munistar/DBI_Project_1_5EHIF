9611a58a5e878b9166845321dcedaf547a8b5513
siddyboy69 <siddhantak016@gmail.com>
2025-11-12 11:51:45 +0100

    spring boot set up

---
+++ OnlineCoursePlatform/src/main/java/at/spengergasse/onlinecourseplatform/OnlineCoursePlatformApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.onlinecourseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCoursePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCoursePlatformApplication.class, args);
+    }
+
+}
